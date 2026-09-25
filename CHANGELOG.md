203966cc23671e2d975498402256a8201ea70c2a
Jenkins <jenkins@regula.by>
2026-09-25 06:01:28 +0300

    3.10.431-nightly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 3.10.424-nightly
+## 3.10.431-nightly
 # 8.0.0
 
 * Initial Release
--- ios/flutter_idv/Package.swift
+++ ios/flutter_idv/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .package(name: "FlutterFramework", path: "../FlutterFramework"),
         .package(
             url: "https://github.com/regulaforensics/IDVSDK-Swift-Package",
-            exact: "3.10.2081-nightly"
+            exact: "3.10.2083-nightly"
         ),
     ],
     targets: [
